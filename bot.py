4d1f0afd35ad1b684040eaf3294f5e1cab56b11c
grantmwilson <development@grantmwilson.com>
2022-02-21 10:16:15 -0500

    Load api token

---
+++ bot.py
@@ -0,0 +1,35 @@
+import json
+import nextcord
+
+import logging
+import os
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
+LOG_LEVEL = logging.DEBUG
+
+# region Logging Setup
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+
+logger = logging.getLogger(SCRIPT_NAME)
+
+if __name__ == "__main__":
+    LOG_FILE = CURRENT_DIRECTORY + "/{}.log".format(SCRIPT_NAME)
+    if os.path.exists(LOG_FILE):
+        try:
+            os.remove(LOG_FILE)
+        except PermissionError:
+            pass
+    logging.basicConfig(level=LOG_LEVEL, filename=LOG_FILE)
+    logger.debug("Module started.")
+    logger.debug("Log file set as: %s", LOG_FILE)
+
+logger.debug("Set current directory as: %s", CURRENT_DIRECTORY)
+# endregion
+
+# region Import Token
+
+with open("credentials.json", "r") as f:
+    json_data = json.load(f)
+    api_token = json_data["api_token"]
+
+print(api_token)
